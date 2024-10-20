a08763c94047832e88cd7427051262e9a1a1b2fe
zhanybaev <zhanybaev1211@gmail.com>
2024-10-20 19:42:55 -0400

    responsive

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
-const str1= 'https://firebasestorage.googleapis.com/v0/b/ginger-restaurant.appspot.com/o/menu%2F1fea6dd8-8326-42eb-8f7e-8917159c6521?alt=media&token=b6f0b5ec-8390-4414-906b-0a8133c0533e'
-console.log(str1==="https://firebasestorage.googleapis.com/v0/b/ginger-restaurant.appspot.com/o/menu%2F1fea6dd8-8326-42eb-8f7e-8917159c6521?alt=media&token=b6f0b5ec-8390-4414-906b-0a8133c0533e")
+const book = document.getElementById("book")
+const modal = document.getElementById("modal")
+const closeBtn = document.querySelector('.close');
+
+book.addEventListener('click', ()=>{
+    modal.style.display="block"
+    document.body.style.overflow = 'hidden'; 
+})
+
+closeBtn.addEventListener('click', function () {
+    modal.style.display = 'none';
+    document.body.style.overflow = 'auto';  // Re-enable scroll
+});
+
+const barBtn = document.getElementById("barBtn")
+const barMenu = document.getElementById("barMenu")
+barBtn.addEventListener("click", ()=>{
+    // barMenu.innerHTML=""
+    // barMenu.innerHTML+=""
+})
